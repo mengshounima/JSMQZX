273b0b853fe6b791e96f516379c530bf1635ba42
yanqin.li <1195407656@qq.com>
2015-12-30 14:58:53 +0800

    用户信息，弹框

--- JSMQZX/UserInfoTableVC.m
+++ JSMQZX/UserInfoTableVC.m
@@ -16,7 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
+    self.title = @"用户信息";
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
     
